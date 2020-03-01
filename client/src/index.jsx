d90638fe1f34e0829abb27385d539fc7564907c7
Joe Yan <chengmu.yan@gmail.com>
2020-03-01 14:49:20 -0500

    Build initial CowList and CowEntry components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const cowListData = [
+  { id: 1, name: "joe", description: "cow named joe" },
+  { id: 2, name: "bob", description: "cow named bob" },
+  { id: 3, name: "charlie", description: "cow named charlie" }
+];
+
 class App extends React.Component {
   render() {
     return (
-    <div>Hello World!</div>
+      <div>
+        <CowList cowList={cowListData} />
+      </div>
     );
   }
 }
 
+const CowList = ({ cowList }) => {
+  return (
+    <div>
+      <div>List of cows:</div>
+      <ol>
+        {cowList.map(e => (
+          <CowEntry cow={e} key={e.id} />
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+const CowEntry = ({ cow }) => {
+  return <li>{cow.name}</li>;
+};
+
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App/>, mountNode);
+ReactDOM.render(<App />, mountNode);

b5e447cb56f4877d30a35d2175e303128b2345d9
Joe Yan <chengmu.yan@gmail.com>
2020-03-01 11:29:52 -0500

    Complete initial setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-app.use(express.static('./client/dist'))
+/* Set up Middleware */
+app.use(express.static("./client/dist"));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+/* Set up Routes */
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/api/cows", (req, res) => {
+  res.send("You're gonna get all the cows");
+  console.log("req body >>> ", req.body);
+});
+
+app.post("/api/cows", (req, res) => res.send("You're gonna gimme a cow"));
+
+/* Start the server */
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
